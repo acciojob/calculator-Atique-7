e38ab107ea28b7dc8a7454c3ed1ed9d089d7652d
Atique-7 <khanatique0000@gmail.com>
2022-12-18 12:36:49 +0530

    calculator

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let display = document.getElementById("input")
+
+let buttons = Array.from(document.getElementsByClassName("button"))
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = ""
+        break
+      case "=":
+        try {
+          display.innerText = eval(display.innerText)
+        } catch {
+          display.innerText = "404"
+        }
+        break
+      default:
+        display.innerText += e.target.innerText
+    }
+  })
+})
