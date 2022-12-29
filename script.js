f609396590ce12468ed41e0afad0c03a1b767e7d
Atique-7 <khanatique0000@gmail.com>
2022-12-29 21:25:21 +0530

    done

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-//your code here
-let display = document.getElementById("input")
+// //your code here
+// let display = document.getElementById("input")
 
-let buttons = Array.from(document.getElementsByClassName("button"))
+// let buttons = Array.from(document.getElementsByClassName("button"))
 
-buttons.map((button) => {
-  button.addEventListener("click", (e) => {
-    switch (e.target.innerText) {
-      case "C":
-        display.innerText = ""
-        break
-      case "=":
-        try {
-          display.innerText = eval(display.innerText)
-        } catch {
-          display.innerText = "404"
-        }
-        break
-      default:
-        display.innerText += e.target.innerText
-    }
-  })
-})
+// buttons.map((button) => {
+//   button.addEventListener("click", (e) => {
+//     switch (e.target.innerText) {
+//       case "C":
+//         display.innerText = ""
+//         break
+//       case "=":
+//         try {
+//           display.innerText = eval(display.innerText)
+//         } catch {
+//           display.innerText = "404"
+//         }
+//         break
+//       default:
+//         display.innerText += e.target.innerText
+//     }
+//   })
+// })
+
+let input = document.getElementById("input")
+let expression = ""
+function solve(e) {
+  // console.log(e)
+  if (e === "ans") {
+    // evaluate the expression
+    let result = eval(expression)
+    console.log(result)
+    input.value = result
+  } else if (e === "clear") {
+    expression = ""
+    input.value = ""
+  } else {
+    expression += e
+    console.log(expression)
+    input.value = expression
+  }
+}
